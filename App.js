5bc4195aa6a31fe16f39d806a9a144a1c1585e1f
Aruna Gamage <admgbit@gmail.com>
2019-03-03 12:09:07 +0530

    Button added

--- App.js
+++ App.js
@@ -7,36 +7,33 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React, { Component } from 'react'
-import {
-    ActivityIndicator,
-    AppRegistry,
-    StyleSheet,
-    Text,
-    View,
-} from 'react-native'
+import React, { Component } from 'react';
+import { AppRegistry, View } from 'react-native';
+import { Button } from 'react-native';
 
-export default class App extends Component {
+export default class FlexDimensionsBasics extends Component {
     render() {
         return (
-            <View style={[styles.container, styles.horizontal]}>
-                <ActivityIndicator size="large" color="#0000ff" />
-                
+            // Try removing the `flex: 1` on the parent View.
+            // The parent will not have dimensions, so the children can't expand.
+            // What if you add `height: 300` instead of `flex: 1`?
+            <View style={{flex: 1}}>
+                <View style={{flex: 2, backgroundColor: 'powderblue'}} />
+                <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+                <View style={{flex: 2, backgroundColor: 'yellow'}} />
+                <View style={{flex: 2, backgroundColor: 'skyblue'}} />
+                <View style={{flex: 1, backgroundColor: 'powderblue'}} >
+                    <Button
+                        title="Log In"
+                        color="#841584"
+                        accessibilityLabel="Learn more about this purple button"
+                    />
+
+                </View>
             </View>
-        )
+        );
     }
 }
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center'
-    },
-    horizontal: {
-        flexDirection: 'row',
-        justifyContent: 'space-around',
-        padding: 10
-    }
-})
-
-AppRegistry.registerComponent('App', () => App)
+// skip this line if using Create React Native App
+AppRegistry.registerComponent('AwesomeProject', () => FlexDimensionsBasics);

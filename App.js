2ff16c786c66fc5d726c2dcf9e3012dfe18e2f3d
Aruna Gamage <admgbit@gmail.com>
2019-03-03 13:17:53 +0530

    Customized Layout

--- App.js
+++ App.js
@@ -11,6 +11,8 @@ import React, { Component } from 'react';
 import { AppRegistry, View } from 'react-native';
 import { Button } from 'react-native';
 import { TextInput } from 'react-native';
+import {StyleSheet} from 'react-native';
+
 
 export default class FlexDimensionsBasics extends Component {
     render() {
@@ -21,13 +23,25 @@ export default class FlexDimensionsBasics extends Component {
             <View style={{flex: 1}}>
                 <View style={{flex: 2, backgroundColor: 'powderblue'}} />
                 <View style={{flex: 2, backgroundColor: 'skyblue'}} />
-                <View style={{flex: 2, backgroundColor: 'yellow'}} />
+                <View style={{flex: 1, backgroundColor: 'yellow'}} />
+
                 <View style={{flex: 2, backgroundColor: 'skyblue'}} >
-                    <TextInput
-                        placeholder="User Name"
-                        returnKeyLabel = {"uname"}
-                        onChangeText={(text) => this.setState({text})}
-                    />
+
+                    <View style={styles.textContainer}>
+                        <TextInput
+                            placeholder="User Name"
+                            returnKeyLabel = {"uname"}
+                            onChangeText={(text) => this.setState({text})}
+                        />
+                    </View>
+
+                    <View style={styles.textContainer}>
+                        <TextInput
+                            placeholder="User Name"
+                            returnKeyLabel = {"uname"}
+                            onChangeText={(text) => this.setState({text})}
+                        />
+                    </View>
 
                     <TextInput
                         placeholder="Password"
@@ -36,17 +50,47 @@ export default class FlexDimensionsBasics extends Component {
                     />
                 </View>
                 <View style={{flex: 1, backgroundColor: 'powderblue'}} >
-                    <Button
-                        title="Log In"
-                        color="#841584"
-                        accessibilityLabel="Learn more about this purple button"
-                    />
+                    <View style={styles.buttonContainer}>
+                        <Button
+                            title="Log In"
+                            color="#841584"
+                        />
+                    </View>
+
+                </View>
+
+                <View style={{flex: 1, backgroundColor: 'powderblue'}} >
+                    <View style={styles.buttonContainer}>
+                        <Button
+                            title="Sigin In"
+                            color="#841584"
+                        />
+                    </View>
 
                 </View>
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+    },
+    buttonContainer: {
+        margin: 20
+    },
+    textContainer: {
+        marginLeft: 30,
+        marginRight: 30
+    },
+    alternativeLayoutButtonContainer: {
+        margin: 20,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    }
+});
 
 // skip this line if using Create React Native App
 AppRegistry.registerComponent('AwesomeProject', () => FlexDimensionsBasics);

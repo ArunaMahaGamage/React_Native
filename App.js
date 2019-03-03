bd756f85ee657e4119bac2c4d4e739e6fee9af76
Aruna Gamage <admgbit@gmail.com>
2019-03-03 09:53:41 +0530

    ActivityIndicator

--- App.js
+++ App.js
@@ -7,54 +7,36 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React from 'react';
-import { FlatList, ActivityIndicator, Text, View  } from 'react-native';
+import React, { Component } from 'react'
+import {
+    ActivityIndicator,
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View,
+} from 'react-native'
 
-export default class FetchExample extends React.Component {
-
-    constructor(props){
-        super(props);
-        this.state ={ isLoading: true}
-    }
-
-    componentDidMount(){
-        return fetch('https://facebook.github.io/react-native/movies.json')
-            .then((response) => response.json())
-            .then((responseJson) => {
-
-                this.setState({
-                    isLoading: false,
-                    dataSource: responseJson.movies,
-                }, function(){
-
-                });
-
-            })
-            .catch((error) =>{
-                console.error(error);
-            });
-    }
-
-
-
-    render(){
-
-        if(this.state.isLoading){
-            return(
-                <View style={{flex: 1, padding: 20}}>
-                    <ActivityIndicator/>
-                </View>
-            )
-        }
-
-        return(
-            <View style={{flex: 1, paddingTop:20}}>
-                <FlatList
-                    data={this.state.dataSource}
-                    renderItem={({item}) => <Text>{item.title}, {item.releaseYear}</Text>}
-                    keyExtractor={({id}, index) => id}
-                />
+export default class App extends Component {
+    render() {
+        return (
+            <View style={[styles.container, styles.horizontal]}>
+                <ActivityIndicator size="large" color="#0000ff" />
+                
             </View>
-        );
+        )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    horizontal: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        padding: 10
+    }
+})
+
+AppRegistry.registerComponent('App', () => App)

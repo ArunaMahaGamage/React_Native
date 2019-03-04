1b25c78f88ec78e0a8bd9be42ee99b6cd6ca27b5
Aruna Gamage <admgbit@gmail.com>
2019-03-04 08:24:24 +0530

    Added Map

--- Home.js
+++ Home.js
@@ -9,11 +9,26 @@
 
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import MapView from 'react-native-maps';
+
 
 export default class Home extends React.Component {
     render() {
         return (
             <View style={styles.container}>
+
+                <View style={[{ width: "90%", height: "90%", margin: 2, backgroundColor: "red" }]}>
+                    <MapView
+                        style={{flex : 1}}
+                        initialRegion={{
+                            latitude: 37.78825,
+                            longitude: -122.4324,
+                            latitudeDelta: 0.0922,
+                            longitudeDelta: 0.0421,
+                        }}
+                    />
+                </View>
+
                 <Text>Home !</Text>
 
                 <Button

db8d1e544fb81954ef0e725c068e8e974fc2b812
Aruna Gamage <admgbit@gmail.com>
2019-03-04 22:53:34 +0530

    Added Map Picker Correctly

--- Home.js
+++ Home.js
@@ -7,31 +7,70 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  TouchableOpacity
+} from 'react-native';
+import RNGooglePlacePicker from 'react-native-google-place-picker';
 
+
+type Props = {};
 export default class Home extends React.Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>Home !</Text>
 
-                <Button
-                    title="Home Add some friends"
-                    onPress={() =>
-                        this.props.navigation.navigate('Friends')
-                    }
-                />
-            </View>
-        );
+constructor(props) {
+    super(props);
+    this.state = {
+      location: null
     }
+  }
+
+onPress() {
+    RNGooglePlacePicker.show((response) => {
+      if (response.didCancel) {
+        console.log('User cancelled GooglePlacePicker');
+      }
+      else if (response.error) {
+        console.log('GooglePlacePicker Error: ', response.error);
+      }
+      else {
+        this.setState({
+          location: response
+        });
+      }
+    })
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity onPress={this.onPress.bind(this)}>
+          <Text style={{color: '#72c02c', fontSize: 20, fontWeight:'bold'}}>
+            Click me to push Google Place Picker!
+          </Text>
+        </TouchableOpacity>
+        <View style={styles.location}>
+          <Text style={{color: 'black', fontSize: 15}}>
+            {JSON.stringify(this.state)}
+          </Text>
+        </View>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  location: {
+    backgroundColor: 'white',
+    margin: 25
+  }
 });
